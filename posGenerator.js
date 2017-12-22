0d1d13da2e5c7bc265d5a5d6d45e4dd09a48da9f
Julia Bristow <julia_bristow@gwu.edu>
2017-12-21 20:51:19 -0500

    add initial button set up

--- posGenerator.js
+++ posGenerator.js
@@ -1,3 +1,4 @@
+//selects a random adjective
 function getAdj ()
 {
 	var adjectives = ['a kindred', 'a beautiful', 'a dazzling', 'an elegant', 'a fancy', 'a glamorous', 'a gorgeous', 
@@ -5,19 +6,33 @@ function getAdj ()
 		'a powerful', 'an ambitious', 'a faithful', 'a happy', 'a victorious', 'a zealous', 'a zesty', 'a lively', 'a witty', 
 		'a spectacular', 'a melodic', 'a modern', 'a delicious', 'a sweet', 'a cuddly', 'a smart', 'a sassy', 'a strong', 
 		'a limitless', 'an important', 'an impressive'];
-	var randomNumber = Math.floor(Math.random()*(adjectives.length-1));
+	var randomNumber = Math.floor(Math.random()*(adjectives.length));
 	return adjectives[randomNumber];
 }
 
+//selects a random noun
 function getNoun ()
 {
 	var nouns = ['ace-in-the-hole', 'benefactor', 'catalyst', 'champion', 'connoisseur', 'devotee', 'dreamboat', 'enchantress', 
 		'angel', 'artisan', 'beauty', 'brain', 'dude', 'comrade', 'genius', 'dear', 'doe', 'delight', 'doll', 'dawg', 'princess', 
 		'fashionista', 'garden', 'gem', 'tamale', 'goddess', 'guardian', 'helper', 'hero', 'hotshot', 'go-getter', 'jester', 
 		'spirit', 'kingpin', 'cupcake', 'legend', "lady's man", 'lionheart', 'masterpiece', 'tapestry', 'being', 'animal'];
-	var randomNumber = Math.floor(Math.random()*(nouns.length-1));
+	var randomNumber = Math.floor(Math.random()*(nouns.length));
 	return nouns[randomNumber];
 }
+
+var compliment = "You are " + getAdj() + " " + getNoun();
+
+/*
+var button = document.createElement("button");
+button.innerHTML = "Do Something";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+button.addEventListener ("click", function() {
+  alert("did something");
+});
 
 /*
 if (sadnessLevel===1)
